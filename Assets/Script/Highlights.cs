230294839e0488adbd4058de5e74bbb3703e7762
porotika6 <porotika6@gmail.com>
2026-01-09 18:19:12 +0700

    3

--- Assets/Script/Highlights.cs
+++ Assets/Script/Highlights.cs
@@ -1,27 +1,75 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class ItemHighlight : MonoBehaviour
+public class ItemInteract : MonoBehaviour
 {
+   
+   private Camera cam;
     private SpriteRenderer sr;
+
+    public Transform cameraTarget; // titik tujuan kamera
 
     public Color normalColor = Color.white;
     public Color highlightColor = Color.yellow;
 
+    private bool isHovered;
+
     void Start()
     {
+        cam = Camera.main;
         sr = GetComponent<SpriteRenderer>();
         sr.color = normalColor;
     }
 
-    void OnMouseEnter()
+    void Update()
     {
-        sr.color = highlightColor;
-        Debug.Log("Mouse masuk ke item: " + gameObject.name);
+        DetectHover();
+        DetectClick();
     }
 
-    void OnMouseExit()
+    void DetectHover()
     {
-        sr.color = normalColor;
-        Debug.Log("Mouse keluar dari item: " + gameObject.name);
+        Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
+        Vector2 mouseWorldPos = cam.ScreenToWorldPoint(mouseScreenPos);
+
+        RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
+
+        if (hit.collider != null && hit.collider.gameObject == gameObject)
+        {
+            if (!isHovered)
+            {
+                isHovered = true;
+                sr.color = highlightColor;
+            }
+        }
+        else
+        {
+            if (isHovered)
+            {
+                isHovered = false;
+                sr.color = normalColor;
+            }
+        }
+    }
+
+    void DetectClick()
+    {
+        if (isHovered && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            TeleportCamera();
+        }
+    }
+
+    void TeleportCamera()
+    {
+        if (cameraTarget == null) return;
+
+        cam.transform.position = new Vector3(
+            cameraTarget.position.x,
+            cameraTarget.position.y,
+            cam.transform.position.z // Z kamera tetap
+        );
+
+        Debug.Log("Camera TP ke: " + cameraTarget.position);
     }
 }
